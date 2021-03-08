aa10782ec8bdbbafd10220d9c3e985374ba0b825
Alex-Klionski <leshka.klionski@gmail.com>
2021-03-08 13:28:53 +0300

    Add separator, video, advice

--- app.js
+++ app.js
@@ -25,3 +25,42 @@ function showTime(){
     setTimeout(showTime, 1000)
 }
 showTime()
+
+
+// GOOGLE STATISTICS
+
+google.load("visualization", "1", {packages:["corechart"]});
+google.setOnLoadCallback(drawChart);
+function drawChart() {
+    var data = google.visualization.arrayToDataTable([
+        ['Газ', 'Объём'],
+        ['Еда',     78.09],
+        ['Развлечения', 20.95],
+        ['Магазин',    0.93],
+        ['Квартира  ', 0.03]
+    ]);
+    var options = {
+        title: 'Бюджет',
+        is3D: true,
+        pieResidueSliceLabel: 'Остальное'
+    };
+    var chart = new google.visualization.PieChart(document.getElementById('air'));
+        chart.draw(data, options);
+}
+
+google.setOnLoadCallback(drawChart2);
+function drawChart2() {
+    var data = google.visualization.arrayToDataTable([
+        ['День недели', 'Траты', 'Доход'],
+        ['Понедельник', 1.3, 70],
+        ['Вторник', 2000, 3120],
+        ['Среда', 12170, 9920]
+    ]);
+    var options = {
+        title: 'Траты/Доход',
+        hAxis: {title: 'День недели'},
+        vAxis: {title: 'Бел. рубли'}
+    };
+    var chart = new google.visualization.ColumnChart(document.getElementById('oil'));
+        chart.draw(data, options);
+}
